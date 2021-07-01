df2bf664694b28ce21f01fa67403edc1899473bd
Grestylhea <86292053+Grestylhea@users.noreply.github.com>
2021-07-01 17:41:50 +0700

    Add files via upload

---
+++ ibnu.js
@@ -0,0 +1,6 @@
+function tekan(){
+    const bebas = document.querySelector('#elemen')    
+    const ubahbgc = document.querySelector('body')    
+    bebas.classList.add('hilang')
+    ubahbgc.classList.add('bgc')
+}
